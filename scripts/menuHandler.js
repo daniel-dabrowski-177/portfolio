4a97670de3efd4367797a265f4a7d1ef29f93b05
Caster <daniel.dabrowski.177@gmail.com>
2023-01-02 17:24:32 +0100

    'js-changes'

--- scripts/menuHandler.js
+++ scripts/menuHandler.js
@@ -3,8 +3,11 @@ let nav = document.querySelector(".nav");
 let ul = document.querySelector(".ul");
 let menu = document.querySelector(".menu");
 let logo = document.querySelector(".logo");
+let projects = document.querySelector(".projects");
+let footer = document.querySelector(".footer");
 let isOpen = false;
 
+// Burger click handler
 menu.addEventListener("click", () => {
   if (!isOpen) {
     menu.children[0].attributes[0].textContent =
@@ -21,4 +24,27 @@ menu.addEventListener("click", () => {
     body.style.overflow = "scroll";
     isOpen = false;
   }
+});
+
+// About click handler
+ul.children[0].addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+// Projects click handler
+ul.children[1].addEventListener("click", () => {
+  projects.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+// Contact click handler
+ul.children[2].addEventListener("click", () => {
+  footer.scrollIntoView({
+    behavior: "smooth",
+  });
 });
--- scripts/moreHandler.js
+++ scripts/moreHandler.js
@@ -1,15 +1,23 @@
-const arrow = document.querySelector(".arrow");
-const more = document.querySelector(".more");
-let arrowOpen = false;
+const arrows = document.querySelectorAll(".arrow");
+const moreArray = document.querySelectorAll(".more");
 
-arrow.addEventListener("click", () => {
-  if (!arrowOpen) {
-    more.style.display = "block";
-    arrow.style.rotate = "0deg";
-    arrowOpen = true;
-  } else {
-    more.style.display = "none";
-    arrow.style.rotate = "90deg";
-    arrowOpen = false;
-  }
+// More about project handler
+arrows.forEach((a) => {
+  let arrowOpen = false;
+  a.addEventListener("click", () => {
+    for (let i = 0; i < arrows.length; i++) {
+      arrows[i].parentElement.children[7].style.display = "none";
+    }
+    if (!arrowOpen) {
+      a.parentElement.children[5].textContent = "less info:";
+      a.parentElement.children[7].style.display = "block";
+      a.style.rotate = "0deg";
+      arrowOpen = true;
+    } else {
+      a.parentElement.children[5].textContent = "more info:";
+      a.parentElement.children[7].style.display = "none";
+      a.style.rotate = "90deg";
+      arrowOpen = false;
+    }
+  });
 });
